3900642a9238322f4dc736eace2dd63777feb8a4
Husoid <husoid@mail.ru>
2023-06-12 23:06:30 +0500

    work v2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,19 @@ fun myPay(typeCard: String = "VK pay", amountTransfersInMonth: Int = 0, amountTr
 
     when (typeCard) {
         "MasterCard", "Maestro" ->
-            if (amountTransfersInMonth + amountTransfer > 75000) {
-                commission = ((75000 - amountTransfersInMonth + amountTransfer) * 60 / 100 + 20)
+            if (amountTransfersInMonth > 75000) {
+                commission = amountTransfer * 60 / 100 + 20
             } else {
-                commission = (amountTransfer * 60 / 100 + 20)
+                commission = 0
             }
+
+        "Visa", "Мир" ->
+            if ((amountTransfer * 75 / 100) > 35) {
+                commission = amountTransfer * 75 / 100
+            } else {
+                commission = 35
+            }
+
         "VK pay" -> commission = 0
         else -> commission = 0
     }
